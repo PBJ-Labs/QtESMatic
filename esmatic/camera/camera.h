c13ed81ef3d0bd30d95435595cf586a63bb4d8d3
_MangoLuv’sCode <140750964+PBJ-Labs@users.noreply.github.com>
2025-11-06 13:43:45 -0600

    Create camera.h

---
+++ esmatic/camera/camera.h
@@ -0,0 +1,15 @@
+/*camera.h*/
+
+#ifndef _CAMERA_H_
+#define _CAMERA_H_
+
+#include "iglobals.h"
+#include "math.h"
+#include "plane.h"
+#include "face.h"
+
+#include <gl/GL.h>
+
+class CamWnd : public 
+
+#endif

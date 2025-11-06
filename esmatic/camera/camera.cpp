ae9fb3f9ea9d2d5081e9b9e1b1c58d06222f8c91
_MangoLuv’sCode <140750964+PBJ-Labs@users.noreply.github.com>
2025-11-06 13:55:21 -0600

    Update camera.cpp

--- esmatic/camera/camera.cpp
+++ esmatic/camera/camera.cpp
@@ -2,7 +2,20 @@
 
 #include "camera.h"
 
-CamWnd::CamWnd() : QWidget(g_qesglobal.d_CamWnd)
+//============
+// globals
+int g_nCameraId = 0;
+float g_fOrigin[3] = { 0.0f, 0.0f, 0.0f };
+bool g_bFreeze = false;
+bool g_bBakeLight = false;
+bool g_bFakeLight = false;
+bool g_bFilterBrushes = false;
+bool g_bFilterEntities = false;
+bool g_bFilterTextures = false;
+
+QESGlobals* g_pGetWidget(){};
+
+CamWnd::CamWnd() : QWidget(g_pGetWidget()->d_GLRectangle)
 {
 
 }
